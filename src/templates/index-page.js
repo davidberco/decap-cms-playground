36c870e1401f56b05c0c37133fa9ef054efe377c
David Bercovici <bercovici.david@gmail.com>
2024-02-07 11:17:25 -0500

    removing script

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -70,17 +70,6 @@ export const IndexPageTemplate = ({
           </div>
         </div>
       </section>
-      <script>
-        if (window.netlifyIdentity) {
-          window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-              });
-            }
-          })
-        }
-      </script>
     </div>
   );
 };
