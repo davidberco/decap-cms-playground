a00e1cc61ba1c910878c7ed16a70bad8d539108e
David Bercovici <bercovici.david@gmail.com>
2024-02-07 11:01:06 -0500

    adding script at the end of the body tag in layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -53,6 +53,17 @@ const TemplateWrapper = ({ children }) => {
       <Navbar />
       <div>{children}</div>
       <Footer />
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
     </div>
   );
 };
