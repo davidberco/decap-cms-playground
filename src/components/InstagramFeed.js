b96209e59fa49a189d87f3dd10feecc4d8e1c3b9
David Bercovici <bercovici.david@gmail.com>
2024-05-07 17:53:34 -0500

    test 2 component change on structure of the component

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -2,19 +2,13 @@ import React from 'react'
 
 export const InstagramFeed = () => {
 
-  
-  
   (function(){
-  var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
-  i.src="https://cdn.curator.io/published/d5bfd89f-4d40-487b-b359-3ce282af776a.js";
-  e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-  })();
-
-
+    var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+    i.src="https://cdn.curator.io/published/d5bfd89f-4d40-487b-b359-3ce282af776a.js";
+    e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
 
   return (
-    <div>
-    
-    </div>
+    <div>InstagramFeed</div>
   )
 }
